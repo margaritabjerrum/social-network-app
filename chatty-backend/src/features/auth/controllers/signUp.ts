8fa9deb85d724898200c194ea8df04d94338e44e
margaritabjerrum <margarita.juskyte@gmail.com>
2023-10-06 16:37:28 +0300

    user data to redis cache implemented

--- chatty-backend/src/features/auth/controllers/signUp.ts
+++ chatty-backend/src/features/auth/controllers/signUp.ts
@@ -9,7 +9,10 @@ import { BadRequestError } from '@global/helpers/error-handler';
 import { Helpers } from '@global/helpers/helpers';
 import { UploadApiResponse } from 'cloudinary';
 import { uploads } from '@global/helpers/cloudinary-upload';
+import { IUserDocument } from '@user/interfaces/user.interface';
+import { UserCache } from '@service/redis/user.cache';
 
+const userCache: UserCache = new UserCache();
 
 export class SignUp {
   @joiValidation(signupSchema)
@@ -36,6 +39,11 @@ export class SignUp {
       throw new BadRequestError('File upload: Error occured. Try again.');
     }
 
+    // Add to redis cache
+    const userDataForCache: IUserDocument = SignUp.prototype.userData(authData, userObjectId);
+    userDataForCache.profilePicture = `https://res.cloudinary.com/dux3dgz8f/image/upload/v${result.version}/${userObjectId}`;
+    await userCache.saveUserToCache(`${userObjectId}`, uId, userDataForCache);
+
     res.status(HTTP_STATUS.CREATED).json({ message: 'User created successsfully', authData });
   }
 
@@ -50,5 +58,42 @@ export class SignUp {
       avatarColor,
       createdAt: new Date()
     } as IAuthDocument;
+  }
+
+  private userData(data: IAuthDocument, userObjectId: ObjectId): IUserDocument {
+    const { _id, username, email, uId, password, avatarColor } = data;
+    return {
+      _id: userObjectId,
+      authId: _id,
+      uId,
+      username: Helpers.firstLetterUppercase(username),
+      email,
+      password,
+      avatarColor,
+      profilePicture: '',
+      blocked: [],
+      blockedBy: [],
+      work: '',
+      location: '',
+      school: '',
+      quote: '',
+      bgImageVersion: '',
+      bgImageId: '',
+      followersCount: 0,
+      followingCount: 0,
+      postsCount: 0,
+      notifications: {
+        messages: true,
+        reactions: true,
+        comments: true,
+        follows: true
+      },
+      social: {
+        facebook: '',
+        instagram: '',
+        twitter: '',
+        youtube: ''
+      }
+    } as unknown as IUserDocument;
   }
 }
---
+++ chatty-backend/src/shared/services/redis/user.cache.ts
@@ -0,0 +1,97 @@
+import { IUserDocument } from '@user/interfaces/user.interface';
+import { BaseCache } from './base.cache';
+import Logger from 'bunyan';
+import { config } from '@root/config';
+import { ServerError } from '@global/helpers/error-handler';
+
+
+const log: Logger = config.createLogger('redisConnection');
+
+export class UserCache extends BaseCache {
+  constructor() {
+    super('userCache');
+  }
+
+  public async saveUserToCache(key: string, userUId: string, createdUser: IUserDocument): Promise<void> {
+    const createdAt = new Date();
+    const {
+      _id,
+      username,
+      email,
+      avatarColor,
+      uId,
+      postsCount,
+      work,
+      school,
+      quote,
+      location,
+      blocked,
+      blockedBy,
+      followersCount,
+      followingCount,
+      notifications,
+      social,
+      bgImageVersion,
+      bgImageId,
+      profilePicture
+    } = createdUser;
+    const firstList: string[] = [
+      '_id',
+      `${_id}`,
+      'uId',
+      `${uId}`,
+      'username',
+      `${username}`,
+      'email',
+      `${email}`,
+      'avatarColor',
+      `${avatarColor}`,
+      'createdAt',
+      `${createdAt}`,
+      'postsCount',
+      `${postsCount}`
+    ];
+    const secondList: string[] = [
+      'blocked',
+      JSON.stringify(blocked),
+      'blockedBy',
+      JSON.stringify(blockedBy),
+      'profilePicture',
+      `${profilePicture}`,
+      'followersCount',
+      `${followersCount}`,
+      'followingCount',
+      `${followingCount}`,
+      'notifications',
+      JSON.stringify(notifications),
+      'social',
+      JSON.stringify(social)
+    ];
+    const thirdList: string[] = [
+      'work',
+      `${work}`,
+      'school',
+      `${school}`,
+      'quote',
+      `${quote}`,
+      'location',
+      `${location}`,
+      'bgImageVersion',
+      `${bgImageVersion}`,
+      'bgImageId',
+      `${bgImageId}`,
+    ];
+    const dataToSave: string[] = [...firstList, ...secondList, ...thirdList];
+
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      await this.client.ZADD('user', { score: parseInt(userUId, 10), value: `${key}`});
+      await this.client.HSET(`users:${key}`, dataToSave);
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again');
+    }
+  }
+}
