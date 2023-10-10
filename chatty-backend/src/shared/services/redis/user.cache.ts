d58ab83c17e145777f1fa3409ad31d9c0586b6c9
margaritabjerrum <margarita.juskyte@gmail.com>
2023-10-10 14:04:12 +0300

    style: formating changes

--- chatty-backend/src/shared/services/redis/user.cache.ts
+++ chatty-backend/src/shared/services/redis/user.cache.ts
@@ -5,6 +5,7 @@ import { config } from '@root/config';
 import { ServerError } from '@global/helpers/error-handler';
 import { Helpers } from '@global/helpers/helpers';
 
+
 const log: Logger = config.createLogger('redisConnection');
 
 export class UserCache extends BaseCache {
@@ -79,7 +80,7 @@ export class UserCache extends BaseCache {
       'bgImageVersion',
       `${bgImageVersion}`,
       'bgImageId',
-      `${bgImageId}`
+      `${bgImageId}`,
     ];
     const dataToSave: string[] = [...firstList, ...secondList, ...thirdList];
 
@@ -87,7 +88,7 @@ export class UserCache extends BaseCache {
       if (!this.client.isOpen) {
         await this.client.connect();
       }
-      await this.client.ZADD('user', { score: parseInt(userId, 10), value: `${key}` });
+      await this.client.ZADD('user', { score: parseInt(userId, 10), value: `${key}`});
       await this.client.HSET(`users:${key}`, dataToSave);
     } catch (error) {
       log.error(error);
@@ -101,7 +102,7 @@ export class UserCache extends BaseCache {
         await this.client.connect();
       }
 
-      const response: IUserDocument = (await this.client.HGETALL(`users:${userUId}`)) as unknown as IUserDocument;
+      const response: IUserDocument = await this.client.HGETALL(`users:${userUId}`) as unknown as IUserDocument;
       response.createdAt = new Date(Helpers.parseJson(`${response.createdAt}`));
       response.postsCount = Helpers.parseJson(`${response.postsCount}`);
       response.blocked = Helpers.parseJson(`${response.blocked}`);
