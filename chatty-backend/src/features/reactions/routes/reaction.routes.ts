53c920c83876c5a4d652a017915570416b35e90d
Margarita Bjerrum <116146661+margaritabjerrum@users.noreply.github.com>
2023-10-26 13:59:11 +0300

    feat: remove reaction method implemented

--- chatty-backend/src/features/reactions/routes/reaction.routes.ts
+++ chatty-backend/src/features/reactions/routes/reaction.routes.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import { Add } from '@reaction/controllers/add-reactions';
 import { authMiddleware } from '@global/helpers/auth.middleware';
+import { Remove } from '@reaction/controllers/remove-reaction';
 
 class ReactionRoutes {
   private router: Router;
@@ -11,6 +12,12 @@ class ReactionRoutes {
 
   public routes(): Router {
     this.router.post('/post/reaction', authMiddleware.checkAuthentication, Add.prototype.reaction);
+
+    this.router.delete(
+      '/post/reaction/:postId/:previousReaction/:postReactions',
+      authMiddleware.checkAuthentication,
+      Remove.prototype.reaction
+    );
 
     return this.router;
   }
