82f722dca611512a5e2985161f502bc40fceae74
Margarita Bjerrum <116146661+margaritabjerrum@users.noreply.github.com>
2023-11-08 17:03:25 +0200

    feat: follow and unfollow user methods implemented

--- chatty-backend/src/features/followers/controllers/follow-user.ts
+++ chatty-backend/src/features/followers/controllers/follow-user.ts
@@ -1,11 +1,12 @@
 import { IFollowerData } from '@follower/interfaces/follower.interface';
+import { followerQueue } from '@service/queues/follower.queue';
 import { FollowerCache } from '@service/redis/follower.cache';
 import { UserCache } from '@service/redis/user.cache';
 import { socketIOFollowerObject } from '@socket/follower';
 import { IUserDocument } from '@user/interfaces/user.interface';
 import { Request, Response } from 'express';
 import HTTP_STATUS from 'http-status-codes';
-// import { ObjectId } from 'mongodb';
+import { ObjectId } from 'mongodb';
 import mongoose from 'mongoose';
 
 const followerCache: FollowerCache = new FollowerCache();
@@ -13,26 +14,30 @@ const userCache: UserCache = new UserCache();
 
 export class Add {
   public async follower(req: Request, res: Response): Promise<void> {
-    const { followerID } = req.params;
+    const { followerId } = req.params;
     // update count in cache
-    const followersCount: Promise<void> = followerCache.updateFollowersCountInCache(`${followerID}`, 'followersCount', 1);
-    const followeeCount: Promise<void> = followerCache.updateFollowersCountInCache(`${req.currentUser?.userId}`, 'followingCount', 1);
+    const followersCount: Promise<void> = followerCache.updateFollowersCountInCache(`${followerId}`, 'followersCount', 1);
+    const followeeCount: Promise<void> = followerCache.updateFollowersCountInCache(`${req.currentUser!.userId}`, 'followingCount', 1);
     await Promise.all([followersCount, followeeCount]);
 
-    const cachedFollower: Promise<IUserDocument> = userCache.getUserFromCache(followerID) as Promise<IUserDocument>;
-    const cachedFollowee: Promise<IUserDocument> = userCache.getUserFromCache(`${req.currentUser?.userId}`) as Promise<IUserDocument>;
+    const cachedFollower: Promise<IUserDocument> = userCache.getUserFromCache(followerId) as Promise<IUserDocument>;
+    const cachedFollowee: Promise<IUserDocument> = userCache.getUserFromCache(`${req.currentUser!.userId}`) as Promise<IUserDocument>;
     const response: [IUserDocument, IUserDocument] = await Promise.all([cachedFollower, cachedFollowee]);
 
-    // const followerObjectId: ObjectId = new ObjectId();
+    const followerObjectId: ObjectId = new ObjectId();
     const addFolloweeData: IFollowerData = Add.prototype.userData(response[0]);
     socketIOFollowerObject.emit('add follower', addFolloweeData);
 
-    const addFollowerToCache: Promise<void> = followerCache.saveFollowerToCache(`followers:${req.currentUser?.userId}`, `${followerID}`);
-    const addFolloweeToCache: Promise<void> = followerCache.saveFollowerToCache(`following:${followerID}`, `${req.currentUser?.userId}`);
+    const addFollowerToCache: Promise<void> = followerCache.saveFollowerToCache(`following:${req.currentUser!.userId}`, `${followerId}`);
+    const addFolloweeToCache: Promise<void> = followerCache.saveFollowerToCache(`followers:${followerId}`, `${req.currentUser!.userId}`);
     await Promise.all([addFollowerToCache, addFolloweeToCache]);
 
-    //send data to queue
-
+    followerQueue.addFollowerJob('addFollowerToDB', {
+      keyOne: `${req.currentUser!.userId}`,
+      keyTwo: `${followerId}`,
+      username: req.currentUser!.username,
+      followerDocumentId: followerObjectId
+    });
     res.status(HTTP_STATUS.OK).json({ message: 'Following user now' });
   }
 
--- chatty-backend/src/features/followers/routes/followerRoutes.ts
+++ chatty-backend/src/features/followers/routes/followerRoutes.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/auth.middleware';
 import { Add } from '@follower/controllers/follow-user';
+import { Remove } from '@follower/controllers/unfollow-user';
 
 class FollowerRoutes {
   private router: Router;
@@ -10,7 +11,8 @@ class FollowerRoutes {
   }
 
   public routes(): Router {
-    this.router.put('/user/follow/:followerID', authMiddleware.checkAuthentication, Add.prototype.follower);
+    this.router.put('/user/follow/:followerId', authMiddleware.checkAuthentication, Add.prototype.follower);
+    this.router.put('/user/unfollow/:followeeId/:followerId', authMiddleware.checkAuthentication, Remove.prototype.follower);
 
     return this.router;
   }
